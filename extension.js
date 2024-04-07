f28ee004d5e6df91f3ce6c5dd8eb3fd5735c7b0c
ReubenKabiti <reubenrk2@gmail.com>
2024-04-07 19:24:21 +0200

    feat: convert to markdown

--- extension.js
+++ extension.js
@@ -1,4 +1,19 @@
 const vscode = require('vscode');
+const fs = require("fs")
+const path = require("path")
+
+function textToMD(text, language) {
+	return `\`\`\`${language}\n${text}\`\`\``;
+}
+
+async function save(text, language) {
+	const md = textToMD(text, language);
+	const cwd = process.cwd()
+	const dir = path.join(cwd, "out.md")
+	
+	// test
+	await fs.promises.writeFile(dir, md);
+}
 
 function activate(context) {
 	const cmd = vscode.commands.registerCommand("pdfycode.code2pdf", () => {
@@ -7,8 +22,9 @@ function activate(context) {
 			const selection = ed.selection;
 			if (!selection.isEmpty) {
 				const selectedText = ed.document.getText(selection);
-				vscode.window.showInformationMessage("Selected text: " + selectedText)
-
+				const fileName = ed.document.fileName;
+				const lang = fileName.split(".").pop();
+				save(selectedText, lang);
 				// your code here bruv
 			}
 		}
